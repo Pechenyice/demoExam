0835b2435b9e1b70f582bf83bcb8e58dc8ce4a57
pechenyice <german.iskhakov@mail.ru>
2021-12-20 18:03:06 +0300

    added app icon

--- attempt-1/src/com/igt/util/BaseForm.java
+++ attempt-1/src/com/igt/util/BaseForm.java
@@ -1,13 +1,27 @@
 package com.igt.util;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 
 public class BaseForm extends JFrame {
     public static String APP_NAME = "TEST 1";
+    public static Image APP_ICON = null;
+
+    static {
+        try {
+            APP_ICON = ImageIO.read(BaseForm.class.getClassLoader().getResource("school_logo.png"));
+        } catch (Exception e) {
+            e.printStackTrace();
+            DialogUtil.showError(null, "Не удалось загрузить иконку приложения");
+        }
+    }
 
     public BaseForm(int width, int height) {
         setTitle(APP_NAME);
+        if(APP_ICON != null) {
+            setIconImage(APP_ICON);
+        }
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setMinimumSize(new Dimension(width, height));
         setLocation(
